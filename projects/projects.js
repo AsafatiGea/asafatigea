5e86eea3d7560fdcc7900feb72acec9f1d9ce3d6
Asafati <2382006@unai.edu>
2026-03-16 17:03:04 +0700

    projek

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Projects page loaded");
+  const btn = document.getElementById("seeMoreBtn");
+  const moreProjects = document.querySelector(".more-projects");
+
+  btn.addEventListener("click", () => {
+    if (moreProjects.style.display === "grid") {
+      moreProjects.style.display = "none";
+      btn.textContent = "See More Projects";
+    } else {
+      moreProjects.style.display = "grid";
+      btn.textContent = "Hide Projects";
+    }
+  });
 });
